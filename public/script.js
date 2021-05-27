93778897059fbc6f7a87879ff3457e4c497e1bf5
Yuet Sheung Jackie Chung <ychu8081@uni.sydney.edu.au>
2021-05-28 02:06:22 +1000

    added design layout and grids

--- public/script.js
+++ public/script.js
@@ -1,30 +1,16 @@
 import Navigation from './component/navigation';
 import './component/tasklist';
 
-const links = document.querySelectorAll('.top-nav > ul > li > a');
-const pages = document.querySelectorAll('.page-container');
+const timerLinks = document.querySelectorAll('.timer-nav > ul > li > a')
+const timerPages = document.querySelectorAll('.timer-page-container')
 
-var nav = new Navigation(links, pages);
-nav.getLinks();
-
+var timerNav = new Navigation(timerLinks, timerPages);
 // For Each loop that allows us to loop through arrays or collections one by one and output each iteration or resulting item from each iteration as a input parameter as a call back function
 // which then will passs down to any other functions 
 // in this case we say for each of the link we want to add a event listener which listens out for the 'click' function and when that click function happens we want a console log out the link to the console
-nav.links.forEach(function(link) {
+timerNav.links.forEach((link) => {
     link.addEventListener('click', function() {
-        let pageId = nav.getHash(link);
-        nav.setPage(pageId);
-    })
-})
-
-const subLinks = document.querySelectorAll('.sub-nav > ul > li > a')
-const subPages = document.querySelectorAll('.sub-page-container')
-
-var subNav = new Navigation(subLinks, subPages);
-
-subNav.links.forEach((link) => {
-    link.addEventListener('click', function() {
-        let pageId = subNav.getHash(link);
-        subNav.setPage(pageId);
+        let pageId = timerNav.getHash(link);
+        timerNav.setPage(pageId);
     })
 })
